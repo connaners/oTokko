f12ea784084c932df6eca49a338a978849c0f4b0
connaners <subscriberohman@gmail.com>
2021-05-30 15:55:38 +0700

    Adding Navigation Bar

--- app/src/main/java/com/plete/otokko/MainActivity.kt
+++ app/src/main/java/com/plete/otokko/MainActivity.kt
@@ -2,10 +2,28 @@ package com.plete.otokko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.add
+import com.plete.otokko.fragment.AkunFragment
+import com.plete.otokko.fragment.HomeFragment
+import com.plete.otokko.fragment.KeranjangFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val fragmentHome: Fragment = HomeFragment()
+    val fragmentAkun: Fragment = AkunFragment()
+    val fragmentKeranjang: Fragment = KeranjangFragment()
+    val fm: FragmentManager = supportFragmentManager
+    val active: Fragment = fragmentHome
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
     }
 }
